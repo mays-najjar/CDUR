0d08be16b3b7cc25c447b11d7c844dfa22206a7c
mays-najjar <mays.najjar2001@gmail.com>
2025-02-10 16:19:49 +0200

    render

--- obj/Debug/net8.0/CDUR.AssemblyInfo.cs
+++ obj/Debug/net8.0/CDUR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDUR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99be90d9f19a1499b6711ee069ab972592850aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08f2aab50151d9ab35a1a0f5c5fee7aa1466dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDUR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDUR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
